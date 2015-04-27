ce9a88b13a87b09745810dd72a241a5ebc4ce32e
victorjones <victo@agilizelabs.com>
2015-04-27 15:14:34 -0400

    Add vendor model and resource model to inventorymanager extension

---
+++ app/code/local/Ecommerceguys/Inventorymanager/Model/Resource/Vendor.php
@@ -0,0 +1,9 @@
+<?php
+
+class Ecommerceguys_Inventorymanager_Model_Resource_Vendor extends Mage_Core_Model_Resource_Db_Abstract
+{
+    public function _construct()
+    {    
+        $this->_init('inventorymanager/vendor', 'vendor_id');
+    }
+}
