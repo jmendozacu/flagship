0fbfaa99d9877c70c55ff7b597ad7e15b3ec9afb
victorjones <victo@agilizelabs.com>
2015-04-29 10:43:29 -0400

    add vendors grid to Inventory manager extension

---
+++ app/code/local/Ecommerceguys/Inventorymanager/Block/Adminhtml/Vendor/Grid.php
@@ -0,0 +1,94 @@
+<?php
+
+class Ecommerceguys_Inventorymanager_Block_Adminhtml_Vendor_Grid extends Mage_Adminhtml_Block_Widget_Grid
+{
+  public function __construct()
+  {
+      parent::__construct();
+      $this->setId('vendorGrid');
+      $this->setDefaultSort('vendor_id');
+      $this->setDefaultDir('ASC');
+      $this->setSaveParametersInSession(true);
+  }
+
+  protected function _prepareCollection()
+  {
+      $collection = Mage::getModel('inventorymanager/vendor')->getCollection();
+      $this->setCollection($collection);
+      return parent::_prepareCollection();
+  }
+
+  protected function _prepareColumns()
+  {
+      $this->addColumn('vendor_id', array(
+          'header'    => Mage::helper('inventorymanager')->__('ID'),
+          'align'     =>'right',
+          'width'     => '50px',
+          'index'     => 'vendor_id',
+      ));
+
+      $this->addColumn('title', array(
+          'header'    => Mage::helper('inventorymanager')->__('Title'),
+          'align'     =>'left',
+          'index'     => 'title',
+      ));
+
+	  
+      $this->addColumn('status', array(
+          'header'    => Mage::helper('inventorymanager')->__('Status'),
+          'align'     => 'left',
+          'width'     => '80px',
+          'index'     => 'status',
+          'type'      => 'options',
+          'options'   => array(
+              1 => 'Enabled',
+              2 => 'Disabled',
+          ),
+      ));
+	  
+        $this->addColumn('action',
+            array(
+                'header'    =>  Mage::helper('inventorymanager')->__('Action'),
+                'width'     => '100',
+                'type'      => 'action',
+                'getter'    => 'getId',
+                'actions'   => array(
+                    array(
+                        'caption'   => Mage::helper('inventorymanager')->__('Edit'),
+                        'url'       => array('base'=> '*/*/edit'),
+                        'field'     => 'id'
+                    )
+                ),
+                'filter'    => false,
+                'sortable'  => false,
+                'index'     => 'stores',
+                'is_system' => true,
+        ));
+		
+		$this->addExportType('*/*/exportCsv', Mage::helper('inventorymanager')->__('CSV'));
+		$this->addExportType('*/*/exportXml', Mage::helper('inventorymanager')->__('XML'));
+	  
+      return parent::_prepareColumns();
+  }
+
+    protected function _prepareMassaction()
+    {
+        $this->setMassactionIdField('vendor_id');
+        $this->getMassactionBlock()->setFormFieldName('inventorymanager');
+
+        $this->getMassactionBlock()->addItem('delete', array(
+             'label'    => Mage::helper('inventorymanager')->__('Delete'),
+             'url'      => $this->getUrl('*/*/massDelete'),
+             'confirm'  => Mage::helper('inventorymanager')->__('Are you sure?')
+        ));
+
+        
+        return $this;
+    }
+
+  public function getRowUrl($row)
+  {
+      return $this->getUrl('*/*/edit', array('id' => $row->getId()));
+  }
+
+}
--- app/code/local/Ecommerceguys/Inventorymanager/controllers/Adminhtml/VendorController.php
+++ app/code/local/Ecommerceguys/Inventorymanager/controllers/Adminhtml/VendorController.php
@@ -1,5 +1,17 @@
 <?php 
 class Ecommerceguys_Inventorymanager_Adminhtml_VendorController extends Mage_Adminhtml_Controller_action
 {
+	
+	protected function _initAction() {
+		$this->loadLayout()
+			->_setActiveMenu('inventorymanager/vendors')
+			->_addBreadcrumb(Mage::helper('adminhtml')->__('Items Manager'), Mage::helper('adminhtml')->__('Item Manager'));
 		
+		return $this;
+	}
+	
+	public function indexAction(){
+		$this->_initAction();
+		$this->renderLayout();
+	}
 }
