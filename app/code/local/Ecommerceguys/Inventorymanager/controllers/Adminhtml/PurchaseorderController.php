b40ebc1fa0212baf2dc824bde3f25c30957a8b0f
victorjones <victo@agilizelabs.com>
2015-04-30 14:51:36 -0400

    Add purchase order grid

--- app/code/local/Ecommerceguys/Inventorymanager/controllers/Adminhtml/PurchaseorderController.php
+++ app/code/local/Ecommerceguys/Inventorymanager/controllers/Adminhtml/PurchaseorderController.php
@@ -1,4 +1,16 @@
 <?php 
 class Ecommerceguys_Inventorymanager_Adminhtml_PurchaseorderController extends Mage_Adminhtml_Controller_action
 {
+	protected function _initAction() {
+		$this->loadLayout()
+			->_setActiveMenu('inventorymanager/purchaseorder')
+			->_addBreadcrumb(Mage::helper('adminhtml')->__('Purchase Order'), Mage::helper('adminhtml')->__('Purchase Order'));
+		
+		return $this;
+	}
+	
+	public function indexAction(){
+		$this->_initAction();
+		$this->renderLayout();
+	}
 }
