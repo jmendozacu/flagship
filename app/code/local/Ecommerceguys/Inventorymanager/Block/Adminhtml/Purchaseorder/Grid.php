c7202a6ec1d789d30848764bdf97b9878dda37ce
victorjones <victo@agilizelabs.com>
2015-05-01 13:40:23 -0400

    Update purchase order grid

--- app/code/local/Ecommerceguys/Inventorymanager/Block/Adminhtml/Purchaseorder/Grid.php
+++ app/code/local/Ecommerceguys/Inventorymanager/Block/Adminhtml/Purchaseorder/Grid.php
@@ -32,17 +32,30 @@ class Ecommerceguys_Inventorymanager_Block_Adminhtml_Purchaseorder_Grid extends 
           'align'     =>'left',
           'index'     => 'vendor_id',
       ));
-
+      $methods = Mage::getSingleton('shipping/config')->getActiveCarriers();
+	  $shippingMethods = array();
+	  foreach($methods as $_code => $_method)
+	 	{
+	 		if(!$_title = Mage::getStoreConfig("carriers/$_code/title")){
+	            $_title = $_code;
+	 		}
+	      	$shippingMethods[$_code] = $_title;
+	 	}
       $this->addColumn('shipping_method', array(
           'header'    => Mage::helper('inventorymanager')->__('Shipping Method'),
           'align'     =>'left',
           'index'     => 'shipping_method',
+          'type'	=> 'options',
+          'options'	=> $shippingMethods
       ));
       
+      $paymentTerms = Mage::getModel('inventorymanager/paymentterms')->getArray();
       $this->addColumn('payment_terms', array(
           'header'    => Mage::helper('inventorymanager')->__('Payment Terms'),
           'align'     =>'left',
           'index'     => 'payment_terms',
+          'type'	=> 'options',
+          'options'	=> $paymentTerms
       ));
       
       $this->addColumn('order_qty', array(
