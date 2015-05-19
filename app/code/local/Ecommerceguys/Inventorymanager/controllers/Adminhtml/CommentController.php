a32c10b94f38bc2f15204054bc85e2b05b74026a
Victor Jones <victor@agilizelabs.com>
2015-05-19 23:57:43 +0530

    add file upload to purchaseorder comments

--- app/code/local/Ecommerceguys/Inventorymanager/controllers/Adminhtml/CommentController.php
+++ app/code/local/Ecommerceguys/Inventorymanager/controllers/Adminhtml/CommentController.php
@@ -7,16 +7,37 @@ class Ecommerceguys_Inventorymanager_Adminhtml_CommentController extends Mage_Ad
 	}
 	
 	public function postAction(){
+		//print_r($_FILES); exit;
 		if($data = $this->getRequest()->getPost()){
+			
+			
+			if(isset($_FILES['send_file']['name']) && $_FILES['send_file']['name'] != '') {
+				try {	
+					$uploader = new Varien_File_Uploader('send_file');
+					//$uploader->setAllowedExtensions(array('jpg','jpeg','gif','png'));
+					$uploader->setAllowRenameFiles(false);
+					
+					$uploader->setFilesDispersion(false);
+					$path = Mage::getBaseDir('media') . DS ."purchaseorder_comments" . DS ;
+					$uploader->save($path, $_FILES['send_file']['name'] );
+					
+				} catch (Exception $e) {
+		      
+		        }
+	  			$data['attachement'] = $_FILES['send_file']['name'];
+			}
+			
 			$model = Mage::getModel('inventorymanager/comment');
 			$model->setData($data);
 			$model->setCreatedTime(now());
 			try {
 				$model->save();
+				Mage::getSingleton('adminhtml/session')->addSuccess(Mage::helper('inventorymanager')->__('Comment is added'));
 			}catch (Exception $e){
-				
+				Mage::getSingleton('adminhtml/session')->addError($e->getMessage());
 			}
 		}
+		$this->_redirect('*/adminhtml_purchaseorder/edit' , array('id'=>$data['po_id']));
 	}
 	
 	public function refreshAction(){
