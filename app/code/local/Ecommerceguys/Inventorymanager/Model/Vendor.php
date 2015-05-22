3ca8c6b8ab93f2d32c1bfedaf360ce5ecd2cc804
Victor Jones <victor@agilizelabs.com>
2015-05-22 23:03:41 +0530

    add vendor login functionality at front

--- app/code/local/Ecommerceguys/Inventorymanager/Model/Vendor.php
+++ app/code/local/Ecommerceguys/Inventorymanager/Model/Vendor.php
@@ -7,4 +7,32 @@ class Ecommerceguys_Inventorymanager_Model_Vendor extends Mage_Core_Model_Abstra
         parent::_construct();
         $this->_init('inventorymanager/vendor');
     }
+    
+    public function checkVendorId($id){
+    	$vendorObject = $this->load($id);
+    	if($vendorObject && $vendorObject->getId()){
+    		return true;
+    	}
+    	return false;
+    }
+    
+    public function authenticate($login, $password){
+    	$this->loadVenderByLogin($login);
+    	if(!$this->validatePassword($password)){
+    		throw Mage::exception('Mage_Core', Mage::helper('customer')->__('Invalid login or password.')
+            );
+    	}
+    	return true;
+    }
+    
+    public function loadVenderByLogin($login){
+    	$this->load($login, "username");
+    }
+    
+    public function validatePassword($password){
+    	if($this->getPassword() === $password){
+    		return true;
+    	}
+    	return false;
+    }
 }
