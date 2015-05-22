8a94295fb7a9359996e67ecf5e08c3b20aadb4e1
Victor Jones <victor@agilizelabs.com>
2015-05-23 01:16:51 +0530

    Fix website filter issue

--- app/code/local/Ecommerceguys/Inventorymanager/Model/Resource/Vendor.php
+++ app/code/local/Ecommerceguys/Inventorymanager/Model/Resource/Vendor.php
@@ -6,4 +6,21 @@ class Ecommerceguys_Inventorymanager_Model_Resource_Vendor extends Mage_Core_Mod
     {    
         $this->_init('inventorymanager/vendor', 'vendor_id');
     }
+    
+    public function getProducts($vendorId){
+    	
+    	$resourceObject = Mage::getSingleton('core/resource');
+    	$productTable = $resourceObject->getTableName('catalog_product_entity');
+    	$vendorProductTable = $resourceObject->getTableName('inventorymanager_vendorproduct');
+    	
+    	$connection = $resourceObject->getConnection('core_read');
+    	
+    	$select = $connection->select()
+                ->from(array("e"=>$productTable))
+                ->join(array("vp"=>$vendorProductTable), "e.entity_id = vp.product_id",array('vendor_id'))
+                ->where("vendor_id = " . $vendorId);  
+         
+    	return $connection->fetchAll($select);
+    	
+    }
 }
