c146aae3a2419797907968a577cc1fbc6e224d87
Victor Jones <victor@agilizelabs.com>
2015-05-28 00:09:05 +0530

    add vendorinfo to left header

---
+++ app/code/local/Ecommerceguys/Inventorymanager/Block/Vendor/Info.php
@@ -0,0 +1,22 @@
+<?php 
+
+class Ecommerceguys_Inventorymanager_Block_Vendor_Info extends Mage_Core_Block_Template
+{
+	public function getCurrentVendorId(){
+		$vendor = Mage::getSingleton('core/session')->getVendor();
+		if($vendor && $vendor->getId()){
+			return $vendor->getId();
+		}else{
+			return false;
+		}
+	}
+	
+	public function getCurrentVendor(){
+		$vendor = Mage::getSingleton('core/session')->getVendor();
+		if($vendor && $vendor->getId()){
+			return $vendor;
+		}else{
+			return false;
+		}
+	}
+}
