d6bf154215fd7a3a7d772e7d2363df7e781ed597
Victor Jones <victor@agilizelabs.com>
2015-06-03 23:22:19 +0530

    product information tabs

--- app/code/local/Ecommerceguys/Inventorymanager/controllers/Vendor/ProductController.php
+++ app/code/local/Ecommerceguys/Inventorymanager/controllers/Vendor/ProductController.php
@@ -5,4 +5,24 @@ class Ecommerceguys_Inventorymanager_Vendor_ProductController extends Mage_Core_
 		$this->loadLayout();
 		$this->renderLayout();
 	}
+	
+	public function uploadAction(){
+		if(isset($_FILES['upl']) && $_FILES['upl']['error'] == 0){
+			try {	
+				$uploader = new Varien_File_Uploader('upl');
+           		//$uploader->setAllowedExtensions(array('jpg','jpeg','gif','png'));
+				$uploader->setAllowRenameFiles(false);
+				$uploader->setFilesDispersion(false);
+				$path = Mage::getBaseDir('media') . DS . "uploads" . DS ;
+				$uploader->save($path, $_FILES['upl']['name'] );
+				//echo Mage::helper('inventorymanager')->__("File Uploaded");
+				echo 1;
+				exit;
+			} catch (Exception $e) {
+				echo 2;
+	      		//echo Mage::helper('inventorymanager')->__("File uploading fail");
+	        }
+		}
+		exit;
+	}
 }
