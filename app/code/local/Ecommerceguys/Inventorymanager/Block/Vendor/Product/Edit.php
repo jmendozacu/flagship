9de6503051497c51adb2f238d00493f2b3229ce5
Victor Jones <victor@agilizelabs.com>
2015-06-05 21:31:22 +0530

    fill productinfo form data

--- app/code/local/Ecommerceguys/Inventorymanager/Block/Vendor/Product/Edit.php
+++ app/code/local/Ecommerceguys/Inventorymanager/Block/Vendor/Product/Edit.php
@@ -10,4 +10,74 @@ class Ecommerceguys_Inventorymanager_Block_Vendor_Product_Edit extends Mage_Core
 		$vendorId = Mage::getSingleton('core/session')->getVendor()->getId();
 		return Mage::getModel('inventorymanager/vendor')->load($vendorId);
 	}
+	
+	public function getProductInfoObject(){
+		$vendorId = Mage::getSingleton('core/session')->getVendor()->getId();
+		$productId = $this->getRequest()->getParam('id',0);
+		
+		$productInfoCollection = Mage::getModel('inventorymanager/vendor_productinfo')->getCollection();
+		$productInfoCollection->addFieldToFilter("product_id", $productId);
+		$productInfoCollection->addFieldToFilter("vendor_id", $vendorId);
+		
+		if($productInfoCollection->count() > 0){
+			return $productInfoCollection->getFirstItem();
+		}
+		return false;
+	}
+	
+	public function getProductInfoDescription(){
+		if($productInfoObject = $this->getProductInfoObject()){
+			return $productInfoObject->getDescription();
+		}
+		return false;
+	}
+	
+	public function getProductInfoCost(){
+		if($productInfoObject = $this->getProductInfoObject()){
+			return $productInfoObject->getCost();
+		}
+		return false;
+	}
+	
+	public function getProductInfoLength(){
+		if($productInfoObject = $this->getProductInfoObject()){
+			return $productInfoObject->getLength();
+		}
+		return false;
+	}
+	
+	public function getProductInfoWidth(){
+		if($productInfoObject = $this->getProductInfoObject()){
+			return $productInfoObject->getWidth();
+		}
+		return false;
+	}
+	
+	public function getProductInfoHeight(){
+		if($productInfoObject = $this->getProductInfoObject()){
+			return $productInfoObject->getHeight();
+		}
+		return false;
+	}
+	
+	public function getProductInfoFunSpec(){
+		if($productInfoObject = $this->getProductInfoObject()){
+			return $productInfoObject->getFunSpec();
+		}
+		return false;
+	}
+	
+	public function getProductInfoMaterial(){
+		if($productInfoObject = $this->getProductInfoObject()){
+			return $productInfoObject->getMaterial();
+		}
+		return false;
+	}
+	
+	public function getProductInfoLighting(){
+		if($productInfoObject = $this->getProductInfoObject()){
+			return $productInfoObject->getLighting();
+		}
+		return false;
+	}
 }
