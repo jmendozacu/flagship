ed52cc409b7d692cd552e4ccedf693520919f31a
victorjones <victo@agilizelabs.com>
2015-06-10 09:14:09 -0400

    new design

---
+++ app/code/community/Manv/Responsivebannerslider/Model/Mysql4/Responsivebanner/Collection.php
@@ -0,0 +1,12 @@
+<?php
+    class Manv_Responsivebannerslider_Model_Mysql4_Responsivebanner_Collection extends Mage_Core_Model_Mysql4_Collection_Abstract
+    {
+
+		public function _construct(){
+			$this->_init("responsivebannerslider/responsivebanner");
+		}
+
+		
+
+    }
+	 
