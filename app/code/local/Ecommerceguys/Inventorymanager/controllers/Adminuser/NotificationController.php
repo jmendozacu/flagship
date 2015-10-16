2302c69e2acdc662ddb9e61399a3c2daa757da72
Victor Jones <victor@agilizelabs.com>
2015-10-16 23:09:24 +0530

    notifictions for agent - not done

---
+++ app/code/local/Ecommerceguys/Inventorymanager/controllers/Adminuser/NotificationController.php
@@ -0,0 +1,9 @@
+<?php
+
+class Ecommerceguys_Inventorymanager_Adminuser_NotificationController extends Mage_Core_Controller_Front_Action
+{
+	public function indexAction(){
+		$this->loadLayout();
+		$this->renderLayout();
+	}
+}
