a2bab5be5f195f6a4f76e808c0756c0540829c6f
Victor Jones <victor@agilizelabs.com>
2015-11-20 23:51:26 +0530

    ship manager -> ship screen development Task 69

---
+++ app/code/local/Ecommerceguys/Inventorymanager/controllers/Adminuser/ShipmanagerController.php
@@ -0,0 +1,8 @@
+<?php
+class Ecommerceguys_Inventorymanager_Adminuser_ShipmanagerController extends Mage_Core_Controller_Front_Action
+{
+	public function indexAction(){
+		$this->loadLayout();
+		$this->renderLayout();
+	}
+}
