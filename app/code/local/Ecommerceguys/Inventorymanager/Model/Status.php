37beb95d51fe300347807936e60cbe6ab268ec7c
Victor Jones <victor@agilizelabs.com>
2015-12-21 18:36:01 +0530

    Wording (order status, agent) #104

--- app/code/local/Ecommerceguys/Inventorymanager/Model/Status.php
+++ app/code/local/Ecommerceguys/Inventorymanager/Model/Status.php
@@ -6,8 +6,8 @@ class Ecommerceguys_Inventorymanager_Model_Status
 	public function toOptionArray(){
 		return array(
 			array('value'=>'processing', 'label'=>Mage::helper('inventorymanager')->__("Processing")),
-			array('value'=>'partial', 'label'=>Mage::helper('inventorymanager')->__("Partial Shipped")),
-			array('value'=>'complete', 'label'=>Mage::helper('inventorymanager')->__("Fully Shipped")),
+			array('value'=>'partial', 'label'=>Mage::helper('inventorymanager')->__("Partially Shipped")),
+			array('value'=>'complete', 'label'=>Mage::helper('inventorymanager')->__("Completely Shipped")),
 			array('value'=>'received', 'label'=>Mage::helper('inventorymanager')->__("Received")),
 			array('value'=>'returned', 'label'=>Mage::helper('inventorymanager')->__("Returned")),
 		);
@@ -16,8 +16,8 @@ class Ecommerceguys_Inventorymanager_Model_Status
 	public function getValues(){
 		return array(
 			'processing'=> Mage::helper('inventorymanager')->__("Processing"),
-			'partial'=> Mage::helper('inventorymanager')->__("Partial Shipped"),
-			'complete'=> Mage::helper('inventorymanager')->__("Fully Shipped"),
+			'partial'=> Mage::helper('inventorymanager')->__("Partially Shipped"),
+			'complete'=> Mage::helper('inventorymanager')->__("Completely Shipped"),
 			'received'=> Mage::helper('inventorymanager')->__("Received"),
 			'returned'=> Mage::helper('inventorymanager')->__("Returned"),
 		);
