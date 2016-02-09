e4cf78b9b277c78d05a6778a8e450b476279b023
Victor Jones <victor@agilizelabs.com>
2016-02-09 23:46:41 +0530

    Agent purchase order prices are not updating the correct price #156

--- app/code/local/Ecommerceguys/Inventorymanager/Block/User/Purchaseorder/Addproduct.php
+++ app/code/local/Ecommerceguys/Inventorymanager/Block/User/Purchaseorder/Addproduct.php
@@ -8,9 +8,11 @@ class Ecommerceguys_Inventorymanager_Block_User_Purchaseorder_Addproduct extends
 	}
 	
 	public function getVendorProduct(){
+		$vendorId = $this->getRequest()->getPost('vendor_id');
 		$product = $this->getproduct();
 		$vendorProducts = Mage::getModel('inventorymanager/vendor_productinfo')->getCollection();
 		$vendorProducts->addFieldToFilter('is_revision', 0);
+		$vendorProducts->addFieldToFilter('vendor_id', $vendorId);
 		$vendorProducts->addFieldToFilter('product_id', $product->getId());
 		if($vendorProducts && $vendorProducts->count() && $vendorProducts->count() > 0){
 			return $vendorProducts->getFirstItem();
