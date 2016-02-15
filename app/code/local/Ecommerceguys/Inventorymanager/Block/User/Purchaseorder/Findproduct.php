77c2432ce5782ca15e9be6a4b12811536d85d567
Victor Jones <victor@agilizelabs.com>
2016-02-15 19:30:48 +0530

    Not all products are showing up to select on a purchase order #162

--- app/code/local/Ecommerceguys/Inventorymanager/Block/User/Purchaseorder/Findproduct.php
+++ app/code/local/Ecommerceguys/Inventorymanager/Block/User/Purchaseorder/Findproduct.php
@@ -10,6 +10,7 @@ class Ecommerceguys_Inventorymanager_Block_User_Purchaseorder_Findproduct extend
 		}else{
 			return null;
 		}*/
+		Mage::app()->setCurrentStore(Mage_Core_Model_App::ADMIN_STORE_ID);
 		$productCollection = Mage::getModel('catalog/product')->getCollection();
 		$productCollection->addAttributeToSelect('sku');
 		$productCollection->addAttributeToSelect('name');
